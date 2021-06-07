287c63536d42680279ee20651c20b471f8dca57b
Martin Molema <martin@molema.org>
2021-06-07 12:03:51 +0200

    All tables

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS  Customers ;
+DROP TABLE IF EXISTS  Categories ;
+DROP TABLE IF EXISTS  OrderDetails ;
+DROP TABLE IF EXISTS  Employees ;
+DROP TABLE IF EXISTS  Products ;
+DROP TABLE IF EXISTS  Customers ;
+
 CREATE TABLE Customers
 (
     CustomerID   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -37,4 +44,42 @@ CREATE TABLE OrderDetails
 );
 
 
+
+CREATE TABLE Orders
+(
+    OrderID    INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    CustomerID INT,
+    EmployeeID INT,
+    OrderDate  DATE,
+    ShipperID  INT
+);
+
+CREATE TABLE Products
+(
+    ProductID   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ProductName NVARCHAR(255),
+    SupplierID  INT,
+    CategoryID  INT,
+    Unit        NVARCHAR(255),
+    Price       FLOAT
+);
+
+CREATE TABLE Shippers
+(
+    ShipperID   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ShipperName NVARCHAR(255),
+    Phone       NVARCHAR(255)
+);
+
+CREATE TABLE Suppliers
+(
+    SupplierID   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    SupplierName NVARCHAR(255),
+    ContactName  NVARCHAR(255),
+    Address      NVARCHAR(255),
+    City         NVARCHAR(255),
+    PostalCode   NVARCHAR(255),
+    Country      NVARCHAR(255),
+    Phone        NVARCHAR(255)
+);
 
