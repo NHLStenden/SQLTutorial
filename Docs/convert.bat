dbe897db0be2e5e6edea53e6c6cf1244788cfcd4
Martin Molema (NHL) <martin.molema@nhlstenden.com>
2021-06-15 08:51:18 +0200

    Tested and improved Windows version of the convert shell script

--- Docs/convert.bat
+++ Docs/convert.bat
@@ -1,2 +1,5 @@
-python3 draw-io-export.py -d generatedImages -s 2 -f PNG Diagrams.drawio 2>/dev/null
+@echo off
+REM Possibly need to change PYTHON3 in PYTHON.
+REM Make Sure the drawio.exe is in the PATH-variable
+python3 draw-io-export.py -d generatedImages -s 2 -f PNG "Diagrams.drawio" 2> nul
 
